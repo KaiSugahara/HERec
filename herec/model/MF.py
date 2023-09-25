c74fb62a50e639ba6f3604d0aefe523d6f419e91
Kai Sugahara <git@ksugahara.com>
2023-09-26 01:52:59 +0900

    Add:

--- herec/model/MF.py
+++ herec/model/MF.py
@@ -7,6 +7,11 @@ class MF(nn.Module):
     user_num: int
     item_num: int
     embedDim: int
+
+    def setup(self):
+
+        self.userEmbed = nn.Embed(num_embeddings=self.user_num, features=self.embedDim)
+        self.itemEmbed = nn.Embed(num_embeddings=self.item_num, features=self.embedDim)
     
     @nn.compact
     def __call__(self, X):
@@ -14,7 +19,14 @@ class MF(nn.Module):
         user_ids = X[:, 0]
         item_ids = X[:, 1]
 
-        U = nn.Embed(num_embeddings=self.user_num, features=self.embedDim)(user_ids)
-        V = nn.Embed(num_embeddings=self.item_num, features=self.embedDim)(item_ids)
+        U = self.userEmbed(user_ids)
+        V = self.itemEmbed(item_ids)
         
         return jnp.sum(U * V, axis=1, keepdims=True)
+    
+    def get_all_scores_by_user_id(self, user_id):
+
+        U = self.userEmbed(jnp.array([user_id]))
+        V = self.itemEmbed(jnp.arange(self.item_num))
+        
+        return jnp.sum(U * V, axis=1)
