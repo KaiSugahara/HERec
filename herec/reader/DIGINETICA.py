e882726a3e09c1da0ff8504be9b6379efc690937
Kai Sugahara <git@ksugahara.com>
2023-09-26 21:24:01 +0900

    Add:

--- herec/reader/DIGINETICA.py
+++ herec/reader/DIGINETICA.py
@@ -1,57 +1,35 @@
 import polars as pl
+from .implicitBase import implicitBase
 from herec.utils import *
 
-class DIGINETICA:
+class DIGINETICA(implicitBase):
 
-    def __init__(self):
+    def read(self):
 
-        # Read
+        # READ
         df_RAW = pl.read_csv(f"{getRepositoryPath()}/dataset/DIGINETICA/train-item-views.csv", separator=";", null_values="NA", new_columns=["session_id", "user_id", "item_id", "timeframe", "eventdate"])
-        df_RAW = df_RAW.drop("user_id")
 
         # Cast Datetime
-        df_RAW = df_RAW.with_columns( pl.col("eventdate").str.strptime(pl.Datetime, "%Y-%m-%d") )
+        df_RAW = df_RAW.with_columns( pl.col("eventdate").str.to_date("%Y-%m-%d") )
 
-        # Sort by session_id & timeframe
-        df_RAW = df_RAW.sort("session_id", "timeframe")
-
-        # Delete items that have been viewed consecutively
-        df_RAW = df_RAW.filter(
-            pl.any_horizontal(
-                (pl.col("session_id") != pl.col("session_id").shift(1)),
-                (pl.col("item_id") != pl.col("item_id").shift(1)),  
-            ).fill_null(True)
+        # Clean User ID
+        df_RAW = df_RAW.group_by("session_id").agg( pl.all() ).with_columns(
+            pl.col("user_id").list.eval(pl.element().drop_nulls()).list.unique()
+        ).filter(
+            pl.col("user_id").list.lengths() > 0
+        ).explode(
+            pl.col("user_id")
+        ).explode(
+            "item_id", "timeframe", "eventdate"
         )
 
-        # Split into TRAIN, VALID, TEST subset
-        df_SUBSET = {
-            "TRAIN": df_RAW.filter(pl.col("eventdate").dt.month() < 4),
-            "VALID": df_RAW.filter(pl.col("eventdate").dt.month() == 4),
-            "TEST": df_RAW.filter(4 < pl.col("eventdate").dt.month()),
-        }
+        # Sort
+        df_RAW = df_RAW.sort("eventdate", "session_id", "timeframe")
 
-        # Make Sequences by session_id
-        df_SUBSET = {
-            name: df.groupby("session_id", maintain_order=True).agg(pl.col("item_id").alias("item_list")).drop("session_id")
-            for name, df in df_SUBSET.items()
-        }
-
-        # Delete sessions with a sequence length of 1
-        df_SUBSET = {
-            name: df.filter( pl.col("item_list").list.lengths() > 1 )
-            for name, df in df_SUBSET.items()
-        }
-
-        # Reset IDs
-        item_ids = pl.concat(df_SUBSET.values()).get_column("item_list").explode().unique(maintain_order=True)
-        item_id_map = dict(zip(item_ids, range(1, len(item_ids)+1)))
-        df_SUBSET = {
-            name: df.with_columns( pl.col("item_list").list.eval(pl.element().map_dict(item_id_map)) )
-            for name, df in df_SUBSET.items()
-        }
+        # Select Columns
+        df_RAW = df_RAW.select( pl.col("user_id"), pl.col("item_id"), pl.col("eventdate").alias("timestamp") )
 
         # Set Variables
         self.df_RAW = df_RAW
-        self.df_SUBSET = df_SUBSET
-        self.item_id_map = item_id_map
-        self.item_num = len(item_id_map) + 1
+
+        return self
