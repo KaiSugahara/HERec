839ab61b1198ec658c23be86d96e6cc6c0bda9c4
Kai Sugahara <git@ksugahara.com>
2023-11-24 20:05:39 +0900

    Add:

--- herec/utils/__init__.py
+++ herec/utils/__init__.py
@@ -4,4 +4,5 @@ from .getLoader import getLoader
 from .getRepositoryPath import getRepositoryPath
 from .getTrainer import getTrainer
 from .hyParamSuggester import hyParamSuggester
+from .restoreModelParams import restoreModelParams
 from .resultLoader import resultLoader
