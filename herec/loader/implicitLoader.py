eba0104760bad082fad1455b5b679deda5adf1b1
Kai Sugahara <git@ksugahara.com>
2023-11-28 15:55:30 +0900

    Merge:

--- herec/loader/implicitLoader.py
+++ herec/loader/implicitLoader.py
@@ -2,7 +2,10 @@ import jax
 import math
 import polars as pl
 
-class ssmLoader:
+class implicitLoader:
+    
+    n_neg: int
+    has_weight: bool
 
     def __init__(self, key, df_DATA, batch_size):
         
@@ -25,7 +28,7 @@ class ssmLoader:
         item_num = df_X["item_id"].n_unique()
 
         # Extract Positive Items
-        df_X = df_X.group_by("user_id", maintain_order=True).agg("item_id").with_columns( pl.col("item_id").list.unique().alias("pos_item_ids") ).explode("item_id")
+        df_X = df_X.group_by("user_id", maintain_order=True).agg("item_id", "pop_weight").with_columns( pl.col("item_id").list.unique().alias("pos_item_ids") ).explode("item_id", "pop_weight")
 
         # Negative Sampling
         for i in range(self.n_neg):
@@ -36,10 +39,12 @@ class ssmLoader:
 
             while df_X.get_column("dup_num").any():
                 
+                # Generate Key for sampling
+                key, subkey = jax.random.split(key)
                 # Sampling
                 df_X = df_X.with_columns(
                     pl.when( pl.col("dup_num") )
-                    .then( pl.arange(0, item_num).sample(self.data_size, with_replacement=True).alias(column_name) )
+                    .then( pl.arange(0, item_num).sample(self.data_size, with_replacement=True, seed=subkey.tolist()[0]).alias(column_name) )
                     .otherwise( pl.col(column_name) )
                 )
                 # Check Duplicates between Positive and Negative Items
@@ -51,9 +56,11 @@ class ssmLoader:
 
         # Convert to Matrix
         self.X = jax.device_put(df_X.select("user_id", "item_id", "^neg_item_id_\d+$").to_numpy())
+        self.Y = jax.device_put(df_X.select("pop_weight").to_numpy()) if self.has_weight else None
         
         # Shuffle rows of X
         self.X = self.X[self.shuffled_indices]
+        self.Y = self.Y[self.shuffled_indices] if self.has_weight else None
 
     def __iter__(self):
         
@@ -73,10 +80,11 @@ class ssmLoader:
             
             # Extract {batch_idx}-th Minibatch
             start_index = self.batch_size * self.batch_idx
-            slice_size = min( self.batch_size, (self.data_size - start_index) )
-            X = jax.lax.dynamic_slice_in_dim(self.X, start_index, slice_size)
+            end_index = self.batch_size * (self.batch_idx + 1)
+            X = self.X[start_index:end_index]
+            Y = self.Y[start_index:end_index] if self.has_weight else None
             
             # Update batch-index
             self.batch_idx += 1
             
-            return X, None
+            return X, Y
