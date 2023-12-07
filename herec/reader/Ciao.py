65a7953e9e88527296c5c79c13c1350fffe1d954
Kai Sugahara <git@ksugahara.com>
2023-12-07 20:34:58 +0900

    Fix:

--- herec/reader/Ciao.py
+++ herec/reader/Ciao.py
@@ -9,7 +9,8 @@ class Ciao(ratingBase):
 
         # Read
         df_RAW = scipy.io.loadmat(f"{getRepositoryPath()}/dataset/Ciao/ciao_with_rating_timestamp/rating_with_timestamp.mat")
-        df_RAW = pl.from_numpy(df_RAW["rating"], schema=["user_id", "item_id", "category", "rating", "helpfulness", "timestamp"])
+        df_RAW = pl.from_numpy(df_RAW["rating"])
+        df_RAW.columns = ["user_id", "item_id", "category", "rating", "helpfulness", "timestamp"]
         df_RAW = df_RAW.sort("timestamp")
 
         # Set Variables
