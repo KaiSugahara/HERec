f159e01f21d6743ad19cdd5b532df49bd3ecc7f2
Kai Sugahara <git@ksugahara.com>
2023-12-20 14:34:19 +0900

    Update: 全体でフィルタリングしない

--- herec/reader/Twitch100K.py
+++ herec/reader/Twitch100K.py
@@ -13,10 +13,6 @@ class Twitch100K(implicitBase):
             pl.col("column_3").alias("item_id"),
             pl.col("column_4").alias("timestamp")
         ).unique()
-        df_RAW = df_RAW.sort("timestamp", "user_id", "item_id")
-        
-        # Keep Only First Interaction for Each User and Item
-        df_RAW = df_RAW.unique( ["user_id", "item_id"], keep="first" )
         
         # Sort
         df_RAW = df_RAW.sort("timestamp", "user_id", "item_id")
