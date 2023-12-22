653c31ad490783c94e20855c71d67f3964196dc2
Kai Sugahara <git@ksugahara.com>
2023-12-22 23:09:20 +0900

    Fix:

--- herec/reader/Twitch100K.py
+++ herec/reader/Twitch100K.py
@@ -8,10 +8,11 @@ class Twitch100K(implicitBase):
 
         # Read
         df_RAW = pl.read_csv(f"{getRepositoryPath()}/dataset/Twitch100K/100k_a.csv", has_header=False)
+        df_RAW = df_RAW.filter( pl.col("column_5") - pl.col("column_4") >= 3 ) # 30分以上視聴
         df_RAW = df_RAW.select(
             pl.col("column_1").alias("user_id"),
             pl.col("column_3").alias("item_id"),
-            pl.col("column_4").alias("timestamp")
+            pl.col("column_5").alias("timestamp")
         ).unique()
         
         # Sort
