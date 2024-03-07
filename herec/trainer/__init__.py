9df015ef1bb932effe112e5d17a752c2144c5e21
Kai Sugahara <git@ksugahara.com>
2024-03-07 14:07:49 +0900

    Clean:

--- herec/trainer/__init__.py
+++ herec/trainer/__init__.py
@@ -1,5 +1,2 @@
-from .bceTrainer import bceTrainer
 from .bprTrainer import bprTrainer
 from .ratingTrainer import ratingTrainer
-from .sessionTrainer import sessionTrainer
-from .ssmTrainer import ssmTrainer
